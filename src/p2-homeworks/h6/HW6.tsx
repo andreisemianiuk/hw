07f607c635ee0f161f728a9b3915b7e5a3152360
andreisemianiuk <semeniukandrei677@gmail.com>
2021-03-11 22:06:25 +0300

    HW6

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -1,40 +1,42 @@
-import React, {useState} from "react";
-import SuperEditableSpan from "./common/c4-SuperEditableSpan/SuperEditableSpan";
-import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
-import {restoreState, saveState} from "./localStorage/localStorage";
+import React, { useState } from 'react'
+import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import { restoreState, saveState } from './localStorage/localStorage'
+import s from './HW6.module.css'
 
 function HW6() {
-    const [value, setValue] = useState<string>("");
-
-    const save = () => {
-        saveState<string>("editable-span-value", value);
-    };
-    const restore = () => {
-        // setValue();
-    };
-
-    return (
-        <div>
-            <hr/>
-            homeworks 6
-
-            {/*should work (должно работать)*/}
-            <div>
-                <SuperEditableSpan
-                    value={value}
-                    onChangeText={setValue}
-                    spanProps={{children: value ? undefined : "enter text..."}}
-                />
-            </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperEditableSpan/>*/}
-            <hr/>
-        </div>
-    );
+  const [value, setValue] = useState<string>('')
+  
+  const save = () => {
+    saveState<string>('editable-span-value', value)
+  }
+  const restore = () => {
+    const savedValue = restoreState<string>('editable-span-value', 'some text')
+    setValue(savedValue)
+  }
+  
+  return (
+    <div>
+      <hr />
+      homeworks 6
+      
+      {/*should work (должно работать)*/}
+      <div>
+        <SuperEditableSpan
+          value={value}
+          onChangeText={setValue}
+          spanProps={{ children: value ? undefined : '...enter text', className: s.span }}
+        />
+      </div>
+      <SuperButton onClick={save}>save</SuperButton>
+      <SuperButton onClick={restore}>restore</SuperButton>
+      
+      <hr />
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeSuperEditableSpan/>*/}
+      <hr />
+    </div>
+  )
 }
 
-export default HW6;
+export default HW6
