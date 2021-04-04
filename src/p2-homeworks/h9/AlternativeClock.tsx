ec398e03604892797dedba967be370969427f21d
andreisemianiuk <semeniukandrei677@gmail.com>
2021-04-04 17:37:08 +0300

    HW9

--- src/p2-homeworks/h9/AlternativeClock.tsx
+++ src/p2-homeworks/h9/AlternativeClock.tsx
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './H9.module.css'
 
 function AlternativeClock() {
+  const [timerId, setTimerId] = useState<number>(0)
+  const [date, setDate] = useState<Date>(new Date())
+  const [show, setShow] = useState<boolean>(false)
+  const [isWorking, setIsWorking] = useState<boolean>(true)
+  
+  useEffect(()=>{
+      const id: number = window.setInterval(() => {
+        setDate(new Date())
+      }, 1000)
+      setTimerId(id)
+  },[isWorking])
+  
+  const stop = () => {
+    clearInterval(timerId)
+    setIsWorking(false)
+  }
+  const start = () => {
+    setIsWorking(true)
+  }
 
-
-    return (
-        <div>
-
-        </div>
-    );
+  const onMouseEnter = () => {
+    setShow(true)
+  }
+  const onMouseLeave = () => {
+    setShow(false)
+  }
+  
+  const fixDate = (num: number) => {
+    if (num < 10) {
+      return `0${num}`
+    }
+    return num
+  }
+  
+  const stringTime = `${fixDate(date.getHours())}:${fixDate(date.getMinutes())}:${fixDate(date.getSeconds())}`
+  const stringDate = `${fixDate(date.getDate())}.${fixDate(date.getMonth()+1)}.${date.getFullYear()}`
+  
+  
+  return (
+    <div className={s.container}>
+      <div
+        className={s.time}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
+        {stringTime}
+      </div>
+      <div className={s.dateWrapper}>
+        {show && (
+          <div className={s.date}>
+            {stringDate}
+          </div>
+        )}
+      </div>
+      <SuperButton onClick={start}>start</SuperButton>
+      <SuperButton onClick={stop}>stop</SuperButton>
+  
+    </div>
+  )
 }
 
-export default AlternativeClock;
+export default AlternativeClock
--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,52 +1,61 @@
-import React, {useState} from "react";
-import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import React, { useState } from 'react'
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './H9.module.css'
 
 function Clock() {
-    const [timerId, setTimerId] = useState<number>(0);
-    const [date, setDate] = useState<Date>();
-    const [show, setShow] = useState<boolean>(false);
-
-    const stop = () => {
-        // stop
+  const [timerId, setTimerId] = useState<number>(0)
+  const [date, setDate] = useState<Date>(new Date())
+  const [show, setShow] = useState<boolean>(false)
+  
+  const stop = () => {
+    clearInterval(timerId)
+  }
+  const start = () => {
+    stop()
+    const id: number = window.setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    setTimerId(id)
+  }
+  
+  const onMouseEnter = () => {
+    setShow(true)
+  }
+  const onMouseLeave = () => {
+    setShow(false)
+  }
+  
+  const fixDate = (num: number) => {
+    if (num < 10) {
+      return `0${num}`
     }
-    const start = () => {
-        stop();
-        const id: number = window.setInterval(() => {
-            // setDate
-        }, 1000);
-        setTimerId(id);
-    }
-
-    const onMouseEnter = () => {
-        // show
-    };
-    const onMouseLeave = () => {
-        // close
-    };
-
-    const stringTime = "Time"; // fix with date
-    const stringDate = "Date"; // fix with date
-
-    return (
-        <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                {stringTime}
-            </div>
-
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
-
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
-
-        </div>
-    );
+    return num
+  }
+  
+  const stringTime = `${fixDate(date.getHours())}:${fixDate(date.getMinutes())}:${fixDate(date.getSeconds())}`
+  const stringDate = `${fixDate(date.getDate())}.${fixDate(date.getMonth()+1)}.${date.getFullYear()}`
+  
+  return (
+    <div className={s.container}>
+      <div
+        className={s.time}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
+        {stringTime}
+      </div>
+      <div className={s.dateWrapper}>
+        {show && (
+          <div className={s.date}>
+            {stringDate}
+          </div>
+        )}
+      </div>
+      <SuperButton onClick={start}>start</SuperButton>
+      <SuperButton onClick={stop}>stop</SuperButton>
+    
+    </div>
+  )
 }
 
-export default Clock;
+export default Clock
